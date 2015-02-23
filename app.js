c90232e82142ba28bc5b3cf5a5f275824cfaf142
murre <mattias.teye@gmail.com>
2015-02-23 12:53:44 +0100

    Express app with InfluxDB and MQTT
    1. open connection to op-en.se with putty or meerkat
    2. type npm install in terminal to install dependencies
    3. type node app.js in terminal
    4. go to localhost:8000 in browser

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var influx 	= require('influx');
 var express = require('express');
+var socket 	= require('socket.io');
+var mqtt 	= require('mqtt');
 var app 	= express();
 var port 	= 8000;
 var client 	= influx({
@@ -9,18 +11,35 @@ var client 	= influx({
 	password: 'root',
 	database: 'Munktell'
 })
-var query = 'select * from "Testsites/MunktellSiencePark/mainmeter/meterevent" limit 10;'
+
+//build a query for influxdb that gets meterevents later than lastTime, unless it's the first query
+function query(lastTime){
+	if(typeof(lastTime) === 'undefined'){
+		return 'select * from "Testsites/MunktellSiencePark/mainmeter/meterevent" limit 100;';
+	}else{
+		return 'select * from "Testsites/MunktellSiencePark/mainmeter/meterevent" ' + 'where time > \'' + lastTime + '\' limit 100;';
+	}
+}
 
 app.set('view engine', 'ejs');
-app.locals.pageTitle = "Energy context awareness"; 
+app.locals.pageTitle = "Energy context awareness";
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-	client.query(query, function(err, data){
+	client.query(query(), function(err, data){
 		if(err!=null){
 			res.send('there was an error\n');
 			console.log(err);
 		}
-		
+
+		//SOCKET.IO MQTT DATA
+		socket.mqtt = mqtt.connect('mqtt://op-en.se:1883');
+		socket.mqtt.subscribe('#');
+		socket.mqtt.on('message', function(topic, message){
+			io.sockets.emit('mqtt',{'topic':String(topic),'payload':String(message)});
+		});
+
+
 		//GOT A RESPONSE
 		var columns = data[0].columns;
 		var points = data[0].points;
@@ -40,3 +59,4 @@ app.get('*', function(req, res){
 var server = app.listen(port, function(){
 	console.log('listening on port ' + port);
 });
+var io = socket.listen(server);
